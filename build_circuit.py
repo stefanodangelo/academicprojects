70c184179f77a4dd70c44360fc1f0fc6ff19f659
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-06 21:19:29 +0200

    Update build_circuit.py

--- build_circuit.py
+++ build_circuit.py
@@ -7,7 +7,15 @@ from .measure import add_measurements
 from math import sqrt
 
 def build_circuit(n_qubits, save_path = 'circuit.json'):
+    message = "Welcome to Orquestra!"
 
+    message_dict = {}
+    message_dict["message"] = message
+    message_dict["schema"] = "message"
+
+    with open("welcome.json",'w') as f:
+        f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
+    """
     # Define registers
     qr = QuantumRegister(n_qubits)
     cr = ClassicalRegister(n_qubits)
@@ -25,4 +33,5 @@ def build_circuit(n_qubits, save_path = 'circuit.json'):
         qc.barrier(qr)
     qc = add_measurements(qc, qr, cr)
     
-    to_zap(qc, save_path)
+    to_zap(qc, save_path
+    """

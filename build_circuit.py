ebb6314e5b2e5140fd23fd5e27ce16b41c24a134
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-08 10:31:28 +0200

    Update build_circuit.py

--- build_circuit.py
+++ build_circuit.py
@@ -23,7 +23,7 @@ import json
 #from qiskit.providers.ibmq import least_busy
 #from qiskit.tools.monitor import job_monitor
 
-def build_circuit(n_qubits, save_path = 'circuit.json'):
+def build_circuit():
     
     message = "Welcome to Orquestra!"
 
@@ -53,47 +53,3 @@ def build_circuit(n_qubits, save_path = 'circuit.json'):
     
     to_zap(qc, save_path)
     """
-def init(qc, qr):
-    qc.h(qr)
-    
-    return qc
-
-def mcz(qc, qr):
-    last_qubit = len(qc.qubits) - 1
-    
-    # barriers only for visualization purposes
-    qc.barrier(qr)
-    qc.barrier(qr)
-    
-    qc.h(last_qubit)
-    qc.mct(qr[:-1], qr[-1])  # multi-controlled-toffoli
-    qc.h(last_qubit)
-    
-    # barriers only for visualization purposes
-    qc.barrier(qr)
-    qc.barrier(qr)
-    
-    return qc
-
-def oracle(qc, qr):
-    
-    qc.x(qr)
-    qc = mcz(qc, qr)
-    qc.x(qr)
-
-    
-    return qc
-
-def diffuser(qc, qr):
-    qc.h(qr)
-    qc.x(qr)
-    qc = mcz(qc, qr)
-    qc.x(qr)
-    qc.h(qr)
-    
-    return qc
-
-def add_measurements(qc, qr, cr):
-    qc.measure(qr, cr)
-    
-    return qc
